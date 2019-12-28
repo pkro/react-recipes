87c0623e214881f64bd6c7253e6a1d3645f69a89
pkro <pkrombach@web.de>
2019-12-28 10:40:22 +0100

    added build path to config

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,10 @@
+const webpack = require('webpack');
+
 module.exports = {
+    entry: './index.js',
+    output: {
+        path: __dirname,
+        filename: 'bundle.js',
+    },
     mode: 'development'
   };
