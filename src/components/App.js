a7d359a3ac4e1acd12333d8884ba719268c87c55
pkro <pkrombach@web.de>
2019-12-29 18:54:17 +0100

    basic recipe loading from webservice

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,10 @@ import Header from './Header';
 import RecipeList from './RecipeList';
 import RecipeDetail from './RecipeDetail';
 
+fetch('http://reactrecipes.herokuapp.com/v1/recipes')
+.then(res => res.json())
+.then(json => console.log(json));
+
 const App = () => {
         return (
             <div>
