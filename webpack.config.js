6fb1009ad4ed868ff76cc94d08046b829e589ecf
pkro <pkrombach@web.de>
2019-12-31 09:48:35 +0100

    linted

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 require('dotenv').config(); // searches for .env file and loads any variable defined there
 
@@ -8,38 +8,45 @@ module.exports = {
     context: path.join(__dirname, 'src'), // source directory so we don't have to add it everywhere below by hand ("root", so still use ./)
     entry: ['whatwg-fetch', './index.js'], // start point(s) from where dependencies are searched
     output: {
-        path: path.resolve(__dirname, 'dist'), // output transpiled to dist folder 
+        path: path.resolve(__dirname, 'dist'), // output transpiled to dist folder
         filename: 'bundle.js',
     },
     module: { // optional modules defined in rules (there can be many objects under rules)
         rules: [
             {
                 test: /\.js$/, // test for this regex
-                exclude: /node_modules/, 
-                loader: 'babel-loader' // ... use this module to preprocess
+                exclude: /node_modules/,
+                loader: 'babel-loader', // ... use this module to preprocess
             },
             {
                 test: /\.(png|jpg|jpeg|gif)$/, // loader for image files
-                loader: 'file-loader' // now, instead of file content, the loader will return the path of the image
+                loader: 'file-loader', // now, instead of file content, the loader will return the path of the image
             },
             {
                 test: /\.css$/, // loader for css
-                loader: ['style-loader', 'css-loader', 'postcss-loader'] // loaders are applied last to first from the array
-                // postcss-loader will transpile, css-loader makes it possible to import styles using @import, and style loader will let webpack pack into index html
-            }
+                loader: ['style-loader', 'css-loader', 'postcss-loader'], // loaders are applied last to first from the array
+                // postcss-loader will transpile, css-loader makes it possible to import styles
+                // using @import, and style loader will let webpack pack into index html
+            },
 
-        ]
+        ],
+    },
+    devServer: {
+        // if url path is not found, redirect to index.html (so the react router can handle it)
+        historyApiFallback: true,
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: './index.html',
-            inject: 'body' // where the script tag gets injected in the html
+            inject: 'body', // where the script tag gets injected in the html
         }),
         new webpack.DefinePlugin({
-            API_URL: JSON.stringify(process.env.API_URL),   // replace API_URL (or any name defined here) with value defined in .env file
-                                                            // we could also just use "process.env.API_URL" in the scripts, this is just more comfortable
+            // replace API_URL (or any name defined here) with value defined in .env file
+            // we could also just use "process.env.API_URL" in the scripts,
+            // this is just more comfortable
+            API_URL: JSON.stringify(process.env.API_URL),
         }),
     ],
-    
-    mode: 'development'
-  };
+
+    mode: 'development',
+};
