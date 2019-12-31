ccfecc3aa076af0f5138aaa2108d33c3197117b0
pkro <pkrombach@web.de>
2019-12-31 10:30:57 +0100

    Added Navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,33 @@
 import React from 'react';
 // import Logo from '../static/images/pkro.png';
+import { NavLink } from 'react-router-dom';
 
 // <img src={Logo} />
+
 const Header = () => (
-    <header className="px4">
+    <header className="flex items-center justify-between px4">
         <h1 className="h1">
             <span role="img" aria-label="food emoji">
                 🍲
             </span>
             MyRecipes
         </h1>
+        <nav>
+            <NavLink
+                exact
+                to="/"
+                className="p1 mx2 black text-decoration-none rounded"
+                activeClassName="bg-white">
+                Home
+            </NavLink>
+            <NavLink
+                exact
+                to="/favorites"
+                className="p1 mx2 black text-decoration-none rounded"
+                activeClassName="bg-white">
+                Favorites
+            </NavLink>
+        </nav>
     </header>
 );
 
