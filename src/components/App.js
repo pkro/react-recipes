51b996695dd4fa285a1dfd69a72ad5270530281b
pkro <pkrombach@web.de>
2019-12-31 09:55:17 +0100

    added NotFound route

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 // https://stackoverflow.com/questions/36795819/when-should-i-use-curly-braces-for-es6-import
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './Home';
 import Favorites from './Favorites';
 import Header from './Header';
+import NotFound from './NotFound';
 
 const App = () => (
     <BrowserRouter>
         <main>
             <Header />
-            <Route exact path="/" component={Home} />
-            <Route path="/favorites" component={Favorites} />
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/favorites" component={Favorites} />
+                <Route component={NotFound} />
+            </Switch>
         </main>
     </BrowserRouter>
 );
